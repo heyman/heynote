d1b61ce899e57a8474a9ccae8f7aa334ab992a02
Jonatan Heyman <jonatan@heyman.info>
2023-01-13 01:40:35 +0100

    Don't auto open developer tools

--- heynote-electron/electron/main/index.ts
+++ heynote-electron/electron/main/index.ts
@@ -58,9 +58,10 @@ async function createWindow() {
   if (process.env.VITE_DEV_SERVER_URL) { // electron-vite-vue#298
     win.loadURL(url)
     // Open devTool if the app is not packaged
-    win.webContents.openDevTools()
+    //win.webContents.openDevTools()
   } else {
     win.loadFile(indexHtml)
+    //win.webContents.openDevTools()
   }
 
   // Test actively push message to the Electron-Renderer
