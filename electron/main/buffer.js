61c6d954e0478f2ca3f7852398b00e43b250a47e
Jonatan Heyman <jonatan@heyman.info>
2023-12-23 11:09:55 +0100

    Do the realpathSync() call for the whole final file path, to allow the buffer file to be a symlink

--- electron/main/buffer.js
+++ electron/main/buffer.js
@@ -5,20 +5,19 @@ import CONFIG from "../config"
 import { isDev } from "../detect-platform"
 
 
-function realpathSync(path) {
-    try {
-        return fs.realpathSync(path);
-    } catch (err) {
-        if (err.code !== "ENOENT") {
-            throw err;
-        }
-    }
-    return "";
-};
-
 export function getBufferFilePath() {
     let defaultPath = app.getPath("userData")
     let configPath = CONFIG.get("settings.bufferPath")
-    let bufferPath = realpathSync(configPath.length ? configPath : defaultPath);
-    return join(bufferPath, isDev ? "buffer-dev.txt" : "buffer.txt")
+    let bufferPath = configPath.length ? configPath : defaultPath
+    let bufferFilePath = join(bufferPath, isDev ? "buffer-dev.txt" : "buffer.txt")
+    try {
+        // use realpathSync to resolve a potential symlink
+        return fs.realpathSync(bufferFilePath)
+    } catch (err) {
+        // realpathSync will fail if the file does not exist, but that doesn't matter since the file will be created
+        if (err.code !== "ENOENT") {
+            throw err
+        }
+        return bufferFilePath
+    }
 }
