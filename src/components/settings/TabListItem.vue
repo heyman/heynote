adab45afdb0cb7a3ce108175c1c6bcf7e6e369a1
Jonatan Heyman <jonatan@heyman.info>
2023-12-28 14:42:09 +0100

    Organize Settings in different tabs

---
+++ src/components/settings/TabListItem.vue
@@ -0,0 +1,34 @@
+<script>
+    export default {
+        props: ["tab", "activeTab", "name"],
+
+        computed: {
+            tabClass() {
+                return this.tab === this.activeTab ? "active" : ""
+            }
+        }
+    }
+</script>
+
+<template>
+    <li :class="tabClass">{{ name }}</li>
+</template>
+
+<style scoped lang="sass">
+    li
+        padding: 9px 20px
+        font-size: 13px
+        user-select: none
+        cursor: pointer
+        &:hover
+            background: #f1f1f1
+            +dark-mode
+                background: #292929
+        &.active
+            background: #48b57e
+            color: #fff
+            cursor: default
+            +dark-mode
+                background: #1b6540
+                
+</style>
