c14c7007912a02fb145a80c43e06efd8df6c99e7
Jonatan Heyman <jonatan@heyman.info>
2024-07-24 12:31:19 +0200

    Add a way to display error dialogs

--- webapp/main.js
+++ webapp/main.js
@@ -1,10 +1,14 @@
 import '../src/css/application.sass'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
 import App from '../src/components/App.vue'
 import { loadCurrencies } from '../src/currency'
 
+const pinia = createPinia()
 const app = createApp(App)
+app.use(pinia)
 app.mount('#app')
 //console.log("test:", app.hej.test)
 
