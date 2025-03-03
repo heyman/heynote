heyman/heynote

commit f1d74bb14f9ce38cb7627a3a8778f1f4009d0c18

Fix escaping issue in buffer selector
Buffer names and paths are now properly escaped.

src/components/BufferSelector.vue
@@ -263,8 +263,8 @@
                         @click="selectItem(item)"
                         ref="item"
                     >
-                        <span class="name" v-html="item.name" />
-                        <span class="path" v-html="item.folder" />
+                        <span class="name">{{ item.name }}</span>
+                        <span class="path">{{ item.folder }}</span>
                         <span :class="{'action-buttons':true, 'visible':actionButton > 0 && idx === selected}">
                             <button 
                                 v-if="actionButton > 0 && idx === selected"

commit ca6addbfa0477be4ada6cd331cdb7995852f4a68

Fix issue with highlighting matched string in the buffer selector
broke in f1d74bb14f9ce38cb7627a3a8778f1f4009d0c18

src/components/BufferSelector.vue
@@ -7,6 +7,15 @@
 
     const pathSep = window.heynote.buffer.pathSeparator
 
+    function escapeHTML(str) {
+        return str
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#039;')
+    }
+
     export default {
         props: {
             headline: String,
@@ -106,8 +115,8 @@
                 this.items = Object.entries(this.buffers).map(([path, metadata]) => {
                     return {
                         "path": path,
-                        "name": metadata?.name || path,
-                        "folder": path.split(pathSep).slice(0, -1).join(pathSep),
+                        "name": escapeHTML(metadata?.name || path),
+                        "folder": escapeHTML(path.split(pathSep).slice(0, -1).join(pathSep)),
                         "scratch": path === SCRATCH_FILE_NAME,
                     }
                 })
@@ -263,8 +272,8 @@
                         @click="selectItem(item)"
                         ref="item"
                     >
-                        <span class="name">{{ item.name }}</span>
-                        <span class="path">{{ item.folder }}</span>
+                        <span class="name" v-html="item.name" />
+                        <span class="path" v-html="item.folder" />
                         <span :class="{'action-buttons':true, 'visible':actionButton > 0 && idx === selected}">
                             <button 
                                 v-if="actionButton > 0 && idx === selected"