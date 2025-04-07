3f7671503d4ec51cbabdf6dae179d73376071c87
Jonatan Heyman <jonatan@heyman.info>
2025-04-07 13:04:23 +0200

    Don't perform editor Select All command if the editor isn't focused (e.g. the Settings modal is open). Fix Editor/Editor Cache teardown/cleanup.

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -22,6 +22,10 @@
                 syntaxTreeDebugContent: null,
                 editor: null,
                 onWindowClose: null,
+                onUndo: null,
+                onRedo: null,
+                onDeleteBlock: null,
+                onSelectAll: null,
             }
         },
 
@@ -39,29 +43,36 @@
             }
             window.heynote.mainProcess.on(WINDOW_CLOSE_EVENT, this.onWindowClose)
 
-            window.heynote.mainProcess.on(UNDO_EVENT, () => {
+            this.onUndo = () => {
                 if (this.editor) {
                     toRaw(this.editor).undo()
                 }
-            })
+            }
+            window.heynote.mainProcess.on(UNDO_EVENT, this.onUndo)
 
-            window.heynote.mainProcess.on(REDO_EVENT, () => {
+            this.onRedo = () => {
                 if (this.editor) {
                     toRaw(this.editor).redo()
                 }
-            })
+            }
+            window.heynote.mainProcess.on(REDO_EVENT, this.onRedo)
             
-            window.heynote.mainProcess.on(DELETE_BLOCK_EVENT, () => {
+            this.onDeleteBlock = () => {
                 if (this.editor) {
                     toRaw(this.editor).deleteActiveBlock()
                 }
-            })
+            }
+            window.heynote.mainProcess.on(DELETE_BLOCK_EVENT, this.onDeleteBlock)
 
-            window.heynote.mainProcess.on(SELECT_ALL_EVENT, () => {
+            this.onSelectAll = () => {
                 if (this.editor) {
-                    toRaw(this.editor).selectAll()
+                    // make sure the editor is focused
+                    if (this.$refs.editor.contains(document.activeElement)) {
+                        toRaw(this.editor).selectAll()
+                    }
                 }
-            })
+            }
+            window.heynote.mainProcess.on(SELECT_ALL_EVENT, this.onSelectAll)
 
             // if debugSyntaxTree prop is set, display syntax tree for debugging
             if (this.debugSyntaxTree) {
@@ -85,10 +96,10 @@
 
         beforeUnmount() {
             window.heynote.mainProcess.off(WINDOW_CLOSE_EVENT, this.onWindowClose)
-            window.heynote.mainProcess.off(UNDO_EVENT)
-            window.heynote.mainProcess.off(REDO_EVENT)
-            window.heynote.mainProcess.off(DELETE_BLOCK_EVENT)
-            window.heynote.mainProcess.off(SELECT_ALL_EVENT)
+            window.heynote.mainProcess.off(UNDO_EVENT, this.onUndo)
+            window.heynote.mainProcess.off(REDO_EVENT, this.onRedo)
+            window.heynote.mainProcess.off(DELETE_BLOCK_EVENT, this.onDeleteBlock)
+            window.heynote.mainProcess.off(SELECT_ALL_EVENT, this.onSelectAll)
             this.editorCacheStore.tearDown();
         },
 
