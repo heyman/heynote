b9b2ffb54904959ab908cbf5af1f916ad14da544
Jonatan Heyman <jonatan@heyman.info>
2025-07-13 23:19:34 +0200

    Change Note -> Buffer teminology

--- src/components/EditBuffer.vue
+++ src/components/EditBuffer.vue
@@ -169,7 +169,7 @@
     <div class="fader" @keydown.stop="onKeydown" tabindex="-1">
         <form class="new-note" tabindex="-1" @focusout="onFocusOut" ref="container" @submit.prevent="submit">
             <div class="container">
-                <h1>Edit Note</h1>
+                <h1>Edit Buffer</h1>
                 <input 
                     placeholder="Name"
                     type="text" 
@@ -191,7 +191,7 @@
                 />
             </div>
             <div class="bottom-bar">
-                <button type="submit">Update Note</button>
+                <button type="submit">Update Buffer</button>
                 <button 
                     class="cancel"
                     @keydown="onCancelKeydown"
