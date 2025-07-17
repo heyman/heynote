aa530e3cc100421ac474ced424fa219ea4a831bd
Jonatan Heyman <jonatan@heyman.info>
2025-07-17 14:12:29 +0200

    Temporarily remove retries in CI to trigger errors in flaky tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
   //workers: process.env.CI ? 1 : undefined,
   workers: process.env.CI ? undefined : undefined,
