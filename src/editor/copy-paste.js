34c9d679fb552cfaddad299e35179e79aa69ad05
Jonatan Heyman <jonatan@heyman.info>
2026-01-15 18:02:04 +0100

    Avoid code duplication for copy/cut

--- src/editor/copy-paste.js
+++ src/editor/copy-paste.js
@@ -40,35 +40,8 @@ function copiedRange(state) {
 export const heynoteCopyCut = (editor) => {
     let copy, cut
     copy = cut = (event, view) => {
-        let { text, ranges } = copiedRange(view.state)
-        text = text.replaceAll(BLOCK_DELIMITER_REGEX, "\n\n")
-        let data = event.clipboardData
-        if (data) {
-            event.preventDefault()
-            data.clearData()
-            data.setData("text/plain", text)
-        }
-        if (event.type == "cut" && !view.state.readOnly) {
-            view.dispatch({
-                changes: ranges,
-                scrollIntoView: true,
-                userEvent: "delete.cut"
-            })
-        }
-        
-        // if we're in Emacs mode, we want to exit mark mode in case we're in it
-        editor.selectionMarkMode = false
-
-        // if Editor.deselectOnCopy is set (e.g. we're in Emacs mode), we want to remove the selection after we've copied the text
-        if (editor.deselectOnCopy && event.type == "copy") {
-            const newSelection = EditorSelection.create(
-                view.state.selection.ranges.map(r => EditorSelection.cursor(r.head)),
-                view.state.selection.mainIndex,
-            )
-            view.dispatch(view.state.update({
-                selection: newSelection,
-            }))
-        }
+        copyCut(editor.view, event.type == "cut", editor)
+        event.preventDefault()
     }
 
     return EditorView.domEventHandlers({
